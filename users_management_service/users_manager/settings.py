872e973c81747dd30ad7b9a2347b35641e0ad1fc
Muhannad7794 <mual0001@stud.kea.dk>
2024-03-13 17:21:46 +0100

    new rebuild with extentions added to the users service container

--- users_management_service/users_manager/settings.py
+++ users_management_service/users_manager/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fp)0$=h&%-l90e7pom*ca2h=z0wwfm3+70^*vzgqqbv%ljxv)+"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -70,8 +74,12 @@ WSGI_APPLICATION = "users_manager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": 3306,
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD")
     }
 }
 
